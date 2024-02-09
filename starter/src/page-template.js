39320452181f58cb6200bcd5c7f0e1ed47091e29
raphaelagil <raphaela.gil@yahoo.com>
2024-02-09 08:47:26 -0300

    configurated the color to the webpage

--- starter/src/page-template.js
+++ starter/src/page-template.js
@@ -4,8 +4,8 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header bg-primary">
+        <div class="card employee-card mr-3">
+        <div class="card-header bg-info">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
@@ -23,8 +23,8 @@ const generateTeam = team => {
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header bg-link">
+        <div class="card employee-card mr-3">
+    <div class="card-header bg-primary">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
@@ -32,19 +32,18 @@ const generateTeam = team => {
         <ul class="list-group">
             <li class="list-group-item">ID: ${engineer.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGitHub()}" target="_blank" rel="noopener noreferrer">${engineer.getGitHub()}</a></li>
         </ul>
     </div>
 </div>
         `;
     };
 
-
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header bg-green">
+        <div class="card employee-card mr-3">
+    <div class="card-header bg-success">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
@@ -101,7 +100,7 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading bg-warning text-white">
+            <div class="col-12 jumbotron mb-3 team-heading bg-info">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
